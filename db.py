56b4ada78858a319549513dafe66aab0e87dc9f7
hurley08 <intriqet@gmail.com>
2024-05-03 00:18:15 -0700

    Sql progress

--- db.py
+++ db.py
@@ -5,37 +5,46 @@ import sqlite3
 conn = sqlite3.connect("legos.db")
 
 
+
+def connect_cursor():
+    c = conn.cursor()
+    return c
+
 def insert_new_piece(
-        'studded'=False, 
-        'units'=5, 
-        'name'="5 studless beam",
-        'description'=None, 
-        'price'=0, 
-        'color'="black"):
-    c.execute(f"""INSERT INTO piece VALUES({studded}, 
-                                            {units}, 
-                                            {name},  
-                                            {description}, 
-                                            {price}, 
-                                            {color})""")
+        studded=False, 
+        units=5, 
+        name="5 studless beam",
+        description=None, 
+        price=0, 
+        color="black"):
+    c.execute(f"""INSERT INTO piece VALUES('{studded}', 
+                                            '{units}', 
+                                            '{name}',  
+                                            '{description}', 
+                                            '{price}', 
+                                            '{color}')""")
 
-c = conn.cursor()
-
-c.execute("""CREATE TABLE pieces (
-        studded binary,
-        units   integer,
-        name    text,
-        description text,
-        price   real,
-        color   text
-)""")
-
-
-# Commit canges to db
-conn.commit()
+def create_table():
+    c.execute("""CREATE TABLE pieces (
+            studded binary,
+            units   integer,
+            name    text,
+            description text,
+            price   real,
+            color   text
+    )""")
 
 
 
-# Close db connection
-conn.close()
 
+def commit_and_close():
+    # Commit canges to db
+    conn.commit()
+    # Close db connection
+    conn.close()
+
+
+if __name__=='__main__':
+    c = connect_cursor()
+    g = c.fetchall()
+    print(g)
