8d1596fec8cda763986a3be9edac71d9e1bb559e
hurley08 <intriqet@gmail.com>
2024-05-06 14:20:40 -0700

    Catching up

--- db/database.py
+++ db/database.py
@@ -2,11 +2,12 @@
 
 import sqlite3
 
-conn = sqlite3.connect("legos.db")
+
 
 
 
 def connect_cursor():
+    conn = sqlite3.connect("legos.db")
     c = conn.cursor()
     return c
 
@@ -25,14 +26,70 @@ def insert_new_piece(
                                             '{color}')""")
 
 def create_table():
-    c.execute("""CREATE TABLE pieces (
-            studded binary,
-            units   integer,
-            name    text,
+
+
+
+
+    c.execute("""CREATE TABLE beams (
+            studded     binary,
+            units_len   integer,
+            units_height integer,
+            units_width real,
+            name        text,
+            angle       integer,
             description text,
-            price   real,
-            color   text
+            price       real,
+            color       char
     )""")
+
+    c.execute("""CREATE TABLE bricks (
+            technic    binary,
+            units_len   integer,
+            units_height integer,
+            units_width real,
+            name        text,
+            description text,
+            price       real,
+            color       char
+    )""")
+
+    c.execute("""CREATE TABLE pins_conns_axle (
+            type     char,
+            units_len   integer,
+            friction    binary,
+            connection  char,
+            adapter     binary,
+            description char,
+            price       real,
+            color       char
+    )""")
+
+    c.execute("""CREATE TABLE gears (
+            style     char,
+            units_diam  real,
+            num_teeth   integer,
+            shaft_type  char,
+            description char,
+            price       real,
+            color       char
+    )""")
+
+
+
+
+def commit_and_close():
+    # Commit canges to db
+    conn.commit()
+    # Close db connection
+    conn.close()
+
+
+if __name__=='__main__':
+    c = connect_cursor()
+    g = c.fetchall()
+    create_table()
+    commit_and_close()
+    print(g)
 
 
 
