a1721ee65d556edaabe20f1981ca6cc91fbd89c3
hurley08 <intriqet@gmail.com>
2024-05-17 12:57:42 -0700

    updating changes

--- db/database.py
+++ db/database.py
@@ -9,7 +9,16 @@ import sqlite3
 def connect_cursor():
     conn = sqlite3.connect("legos.db")
     c = conn.cursor()
-    return c
+    return c, conn
+
+
+def print_all(cur):
+    for row in cur.execute('SELECT * FROM beams;'):
+        print(row)
+    for row in cur.execute('SELECT * FROM bricks;'):
+        print(row)
+    for row in cur.execute('SELECT * FROM pins_conns_axle;'):
+        print(row)
 
 def insert_new_piece(
         studded=False, 
@@ -26,58 +35,65 @@ def insert_new_piece(
                                             '{color}')""")
 
 def create_table():
-g
+    try:    
+        c.execute("""CREATE TABLE beams (
+                studded     binary,
+                units_len   integer,
+                units_height integer,
+                units_width real,
+                name        text,
+                angle       integer,
+                description text,
+                price       real,
+                color       char
+        )""")
+    except:
+        print("beams already exists")
+
+    try:
+        c.execute("""CREATE TABLE bricks (
+                technic    binary,
+                units_len   integer,
+                units_height integer,
+                units_width real,
+                name        text,
+                description text,
+                price       real,
+                color       char
+        )""")
+    except:
+        print("bricks already exists")
+
+    try:
+        c.execute("""CREATE TABLE pins_conns_axle (
+                type     char,
+                units_len   integer,
+                friction    binary,
+                connection  char,
+                adapter     binary,
+                description char,
+                price       real,
+                color       char
+        )""")
+    except:
+        print("pins_conns_axle already exists")
+
+    try:
+        c.execute("""CREATE TABLE gears (
+                style     char,
+                units_diam  real,
+                num_teeth   integer,
+                shaft_type  char,
+                description char,
+                price       real,
+                color       char
+        )""")
+    except:
+            print("gears already exists")
 
 
 
-    c.execute("""CREATE TABLE beams (
-            studded     binary,
-            units_len   integer,
-            units_height integer,
-            units_width real,
-            name        text,
-            angle       integer,
-            description text,
-            price       real,
-            color       char
-    )""")
-
-    c.execute("""CREATE TABLE bricks (
-            technic    binary,
-            units_len   integer,
-            units_height integer,
-            units_width real,
-            name        text,
-            description text,
-            price       real,
-            color       char
-    )""")
-
-    c.execute("""CREATE TABLE pins_conns_axle (
-            type     char,
-            units_len   integer,
-            friction    binary,
-            connection  char,
-            adapter     binary,
-            description char,
-            price       real,
-            color       char
-    )""")
-
-    c.execute("""CREATE TABLE gears (
-            style     char,
-            units_diam  real,
-            num_teeth   integer,
-            shaft_type  char,
-            description char,
-            price       real,
-            color       char
-    )""")
-
-
-
-
-def commit_and_close():
+def commit_and_close(conn):
     # Commit canges to db
     conn.commit()
     # Close db connection
@@ -85,8 +101,9 @@ def commit_and_close():
 
 
 if __name__=='__main__':
-    c = connect_cursor()
+    c, conn = connect_cursor()
     g = c.fetchall()
-    create_table()
-    commit_and_close()
+    #create_table()
+    print_all(c)
+    commit_and_close(conn)
     print(g)
